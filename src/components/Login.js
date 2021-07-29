4ead4bbdb6149fabf492b10cde37f303944d6d8c
Ahmed Skulj <ahmedskulj10@gmail.com>
2021-07-29 19:16:50 +0200

    Started working on design.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import "../App.css";
 const Login = (props) => {
   const {
     email,
@@ -15,41 +15,43 @@ const Login = (props) => {
   } = props;
 
   return (
-    <div>
-      <label>Username</label>
-      <input
-        type="text"
-        required
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <p>{emailErr}</p>
-      <label>Password</label>
-      <input
-        type="password"
-        required
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <p>{passErr}</p>
-      <div>
-        {isLoggedIn ? (
-          <div>
-            <button onClick={handleLogin}>Sign in</button>
-            <p>
-              Register, if you haven't already{" "}
-              <span onClick={() => setIsLoggedIn(!isLoggedIn)}>Register</span>
-            </p>
-          </div>
-        ) : (
-          <div>
-            <button onClick={handleRegister}>Sign up</button>
-            <p>
-              Login if you have an account{" "}
-              <span onClick={() => setIsLoggedIn(!isLoggedIn)}>Login</span>
-            </p>
-          </div>
-        )}
+    <div className="login-container">
+      <div className="login-screen">
+        <label>Username</label>
+        <input
+          type="text"
+          required
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <p>{emailErr}</p>
+        <label>Password</label>
+        <input
+          type="password"
+          required
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <p>{passErr}</p>
+        <div>
+          {isLoggedIn ? (
+            <div>
+              <button onClick={handleLogin}>Sign in</button>
+              <p>
+                Register, if you haven't already{" "}
+                <span onClick={() => setIsLoggedIn(!isLoggedIn)}>Register</span>
+              </p>
+            </div>
+          ) : (
+            <div>
+              <button onClick={handleRegister}>Sign up</button>
+              <p>
+                Login if you have an account{" "}
+                <span onClick={() => setIsLoggedIn(!isLoggedIn)}>Login</span>
+              </p>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
